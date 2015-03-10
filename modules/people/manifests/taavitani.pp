fd66cfe7c3f1f5e6ba013df6fccf8c98308ef907
Taavi Tani <taavi.tani@gmail.com>
2015-03-10 18:21:22 +0200

    Add tmux.

--- modules/people/manifests/taavitani.pp
+++ modules/people/manifests/taavitani.pp
@@ -2,6 +2,7 @@ class people::taavitani {
   include packer
   include iterm2::dev
   include iterm2::colors::solarized_dark
+  include tmux
   class { 'vagrant': }
   atom::package { 'vim-mode': }
 }
